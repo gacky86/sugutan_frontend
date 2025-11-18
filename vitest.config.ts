e427b23921f55cafe786717c5cd47ca300433528
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2025-11-18 12:02:40 +0900

    test: add path alias in vitest.config.ts #25

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   test: {
+    globals: true,
     include: ["src/tests/**/*.test.tsx"],
+    exclude: [
+      // 'src/tests/components/CardEdit.test.tsx',
+      // 'src/tests/components/CardsList.test.tsx',
+      // 'src/tests/components/FlashCardDelete.test.tsx',
+      // 'src/tests/components/FlashCardDetail.test.tsx',
+      // 'src/tests/components/FlashCardSetting.test.tsx',
+      // 'src/tests/components/Home.test.tsx',
+      // 'src/tests/components/NewCard.test.tsx',
+      // 'src/tests/components/NewFlashCard.test.tsx',
+    ],
     environment: "jsdom",
     coverage: {
       reporter: ["text", "github-actions", "json-summary", "json"],
     },
   },
+  plugins: [tsconfigPaths()],
 });
