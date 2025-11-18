df7f4798df49b5de24687de70833df1790530b8e
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2025-11-18 11:48:12 +0900

    test: add path alias in vitest.config.ts #25

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   test: {
     include: ["src/tests/**/*.test.tsx"],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
     environment: "jsdom",
     coverage: {
-      reporter: ["text", "json-summary", "json"],
+      reporter: ["text", "github-actions", "json-summary", "json"],
     },
   },
 });
