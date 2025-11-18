5c836c082e9e854eb738bd711bc8100984986421
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2025-11-18 11:54:08 +0900

    test: add path alias in vitest.config.ts #25

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss(), svgr()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite";
-import path from "path";
 
 export default defineConfig({
   test: {
     include: ["src/tests/**/*.test.tsx"],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "src"),
-      },
-    },
     environment: "jsdom",
     coverage: {
       reporter: ["text", "github-actions", "json-summary", "json"],
