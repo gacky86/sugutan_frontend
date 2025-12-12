5f1efc531fb35ce0244cf8e981905795d931dfad
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2025-12-12 19:15:50 +0900

    refactor: minor change about UI #119

--- src/components/uis/dictionary/uis/WordInfo.tsx
+++ src/components/uis/dictionary/uis/WordInfo.tsx
@@ -7,7 +7,7 @@ type Props = {
 const WordInfo = ({ label, content, subContent }: Props) => {
   return (
     <div className="flex mb-1">
-      <h4 className="min-w-10 whitespace-nowrap">{label}:</h4>
+      <h4 className="min-w-10 whitespace-nowrap mr-3">{label}:</h4>
       <div>
         <p>{content}</p>
         <p>{subContent}</p>
