fc94623013733456be9d5ec4b4f7aa984e95b1eb
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2025-12-19 17:08:51 +0900

    feat: implement sign out feature on signout button #130

---
+++ src/components/uis/common/SignOutButton.tsx
@@ -0,0 +1,25 @@
+import { signOut } from "@/api/auth";
+import { BiLogOut } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
+
+const SignOutButton = () => {
+  const navigate = useNavigate();
+  const handleSigngOut = async () => {
+    try {
+      await signOut();
+    } finally {
+      navigate("signin");
+    }
+  };
+  return (
+    <div
+      className="md:text-xl md:flex md:items-center md:gap-2 md:py-3 md:rounded-lg cursor-pointer duration-300 hover:bg-orange-50"
+      onClick={() => handleSigngOut()}
+    >
+      <BiLogOut />
+      <p className="hidden md:inline">ログアウト</p>
+    </div>
+  );
+};
+
+export default SignOutButton;
